76cc2823c684bf44d59572518b9a94ce4a034115
于新强 <15610538128@163.com>
2023-05-18 13:54:37 +0800

    初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "VuePress 云开发网站应用示例",
+  description: "基于云开发的 VuePress 网站应用示例",
+  base: "/vuepress/",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "指南", link: "/guides/" },
+    ],
+  },
+};
